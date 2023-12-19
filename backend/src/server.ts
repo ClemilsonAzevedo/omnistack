98e9acba1847991ca7a0f447f33e222d89ba2b73
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2023-12-19 10:21:19 +0100

    Initialized project

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,27 +3,8 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 
-const users = ['Clemilson', 'Azevedo', 'Hello']
-
-app.get('/users', (request, response) => {
-  response.json(users)
-})
-
-app.get('/users/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const user = users[id]
-  return response.json(user)
-})
-
-app.post('/users', (request, response) => {
-  const data = request.body
-
-  const user = {
-    name: data.name,
-    email: data.email,
-  }
-
-  return response.json(user)
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World!' })
 })
 
 app.listen(3333)
