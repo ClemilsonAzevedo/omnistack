893156c4eea9dce59bc225b5be6d2b8f8257d2ec
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2023-12-20 10:23:07 +0100

    create two routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,42 @@
 import express from 'express'
+import { connection } from './database/connection'
 
 export const routes = express.Router()
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World!' })
+routes.get('/items', async (request, response) => {
+  const items = await connection.select('*').from('items')
+  const serializedItems = items.map((item) => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    }
+  })
+  return response.json(serializedItems)
+})
+
+routes.post('/points', async (request, response) => {
+  const { name, email, whatsapp, latitude, longitude, city, uf, items } =
+    request.body
+
+  const ids = await connection('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  })
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id: ids[0],
+    }
+  })
+
+  await connection('point_items').insert(pointItems)
+  return response.json({ sucess: true })
 })
